b4e4a37410f6c066879d0f2b4737db9a6f279e00
andeyosera <andeyoseraphina@gmail.com>
2026-03-24 05:06:56 +0300

    fix: add prisma generate to build

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
+  output: 'standalone',
 }
 
 export default nextConfig
